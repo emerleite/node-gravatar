cfc1538682e5d7db9cf69f63007468547006bc94
Tyler Power <tyler@appsecute.com>
2013-01-17 15:48:40 +1300

    Updated to allow falsey emails to be passed through, in which case the default gravater image will be loaded (instead of previous behaviour which caused an excption). Added tests.

--- test/gravatar.test.js
+++ test/gravatar.test.js
@@ -1,32 +1,38 @@
-var should = require('should')
-  , url = require('url')
-  , gravatar = require('../lib/gravatar');
+var should = require('should'),
+    url = require('url'),
+    gravatar = require('../lib/gravatar');
 
-describe('gravatar', function() {
-  var baseURL = "http://www.gravatar.com/avatar/";
-  var baseSecureURL = "https://secure.gravatar.com/avatar/";
+describe('gravatar', function () {
+    var baseURL = "http://www.gravatar.com/avatar/";
+    var baseSecureURL = "https://secure.gravatar.com/avatar/";
 
-  it('should gererate correct uri given an email', function() {
-    gravatar.url('emerleite@gmail.com').should.be.equal(baseURL + "93e9084aa289b7f1f5e4ab6716a56c3b");
-    gravatar.url('emerleite@yahoo.com.br').should.be.equal(baseURL + "6c47672b0d58bd6aae4fa70920cb3ee4");
-  });
+    it('should gererate correct uri given an email', function () {
+        gravatar.url('emerleite@gmail.com').should.be.equal(baseURL + "93e9084aa289b7f1f5e4ab6716a56c3b");
+        gravatar.url('emerleite@yahoo.com.br').should.be.equal(baseURL + "6c47672b0d58bd6aae4fa70920cb3ee4");
+    });
 
-  it('should generate same uri ignoring case', function() {
-    gravatar.url('EMERLEITE@gmAil.com').should.be.equal(baseURL + "93e9084aa289b7f1f5e4ab6716a56c3b");
-    gravatar.url('emerleite@YAHOO.com.BR').should.be.equal(baseURL + "6c47672b0d58bd6aae4fa70920cb3ee4");
-  });
+    it('should generate same uri ignoring case', function () {
+        gravatar.url('EMERLEITE@gmAil.com').should.be.equal(baseURL + "93e9084aa289b7f1f5e4ab6716a56c3b");
+        gravatar.url('emerleite@YAHOO.com.BR').should.be.equal(baseURL + "6c47672b0d58bd6aae4fa70920cb3ee4");
+    });
 
-  it('should generate uri with user passed parameters', function() {
-    var gravatarURL = gravatar.url('emerleite@gmail.com', { s: '200', f: 'y', r: 'g', d: '404'});
-    var queryString = url.parse(gravatarURL, true).query;
-    queryString.s.should.equal('200');
-    queryString.f.should.equal('y');
-    queryString.r.should.equal('g');
-    queryString.d.should.equal('404');
-  });
+    it('should generate uri with user passed parameters', function () {
+        var gravatarURL = gravatar.url('emerleite@gmail.com', { s:'200', f:'y', r:'g', d:'404'});
+        var queryString = url.parse(gravatarURL, true).query;
+        queryString.s.should.equal('200');
+        queryString.f.should.equal('y');
+        queryString.r.should.equal('g');
+        queryString.d.should.equal('404');
+    });
 
-  it('should allow https gravatar uri generation', function() {
-    var gravatarURL = gravatar.url('emerleite@gmail.com', {}, true);
-    gravatar.url('emerleite@gmail.com', {}, true).should.equal(baseSecureURL + "93e9084aa289b7f1f5e4ab6716a56c3b");
-  });
+    it('should allow https gravatar uri generation', function () {
+        var gravatarURL = gravatar.url('emerleite@gmail.com', {}, true);
+        gravatar.url('emerleite@gmail.com', {}, true).should.equal(baseSecureURL + "93e9084aa289b7f1f5e4ab6716a56c3b");
+    });
+
+    it('should handle falsey values for the email property', function () {
+        gravatar.url(null).should.be.ok;
+        gravatar.url(undefined).should.be.ok;
+        gravatar.url('').should.be.ok;
+    });
 });
