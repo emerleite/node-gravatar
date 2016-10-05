2ad1c4c5f4762f6534ed7a3ef4d0bd87e9c30d11
Layton Whiteley <layton.whiteley@gmail.com>
2016-10-05 15:56:28 -0400

    feat: make cli more robust and option rich
    chore: add callback description
    
    chore: use email validator package
    
    add tests
    
    chore: update cli wording
    
    chore: split options
    
    chore: add exit command
    
    optimize code

--- test/gravatar.test.js
+++ test/gravatar.test.js
@@ -96,18 +96,43 @@ describe('gravatar', function() {
 
 describe("CLI", function() {
 
-  it("accepts an email argument and writes gravatar URL to STDOUT", function(done) {
-    nixt()
-      .run('./cli.js zeke@sikelianos.com')
-      .stdout('https://s.gravatar.com/avatar/8f344b1c4bdcfc28bd848e97e94c3523?size=500&default=retro')
-      .end(done)
+  describe("general: ", function() {
+
+    it("accepts an email argument with options and writes gravatar URL to STDOUT", function(done) {
+      nixt()
+        .run('./cli.js zeke@sikelianos.com -p https -s 500 -d retro')
+        .stdout('Gravatar (avatar):\nhttps://s.gravatar.com/avatar/8f344b1c4bdcfc28bd848e97e94c3523?default=retro&size=500')
+        .end(done)
+    })
+    it("outputs usage if -h arg is present", function(done) {
+      nixt()
+        .run('./cli.js -h')
+        .stdout(/Usage/)
+        .end(done)
+    })
+
   })
 
-  it("outputs usage if no arg is present", function(done) {
-    nixt()
-      .run('./cli.js')
-      .stdout(/Usage/)
-      .end(done)
+  describe("avatar command: ", function() {
+
+    it("accepts an email argument with options and writes gravatar URL to STDOUT", function(done) {
+      nixt()
+        .run('./cli.js avatar zeke@sikelianos.com -p https -s 500 -d retro')
+        .stdout('Gravatar (avatar):\nhttps://s.gravatar.com/avatar/8f344b1c4bdcfc28bd848e97e94c3523?default=retro&size=500')
+        .end(done)
+    })
+
+  })
+
+  describe("profile command: ", function() {
+
+    it("accepts an email argument with options and writes gravatar profile URL to STDOUT", function(done) {
+      nixt()
+        .run('./cli.js profile zeke@sikelianos.com -p https -c doSomething')
+        .stdout('Gravatar (profile):\nhttps://secure.gravatar.com/8f344b1c4bdcfc28bd848e97e94c3523.json?callback=doSomething')
+        .end(done)
+    })
+
   })
 
 })
