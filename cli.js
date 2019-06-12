92208fd0ba1de84201ac5ba97ffdbff5aac90b8c
Luka Saric <rpg.ari7@gmail.com>
2019-06-12 12:44:26 +0200

    Refactor
    - Added eslint and editorconfig files ( you can change it as you prefer)
    - Changed 'var' into 'const' or 'let'
    - Decoupled printAvatar into 2 functions for url and for profile
    - Changed if/else into if/ return pattern in most cases
    - String concatination replaced with interpolation in most cases

--- cli.js
+++ cli.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
-var gravatar = require('./lib/gravatar')
-var emailValidator = require("email-validator")
-var mainOpts = {
+const gravatar = require('./lib/gravatar');
+const emailValidator = require('email-validator');
+const path = require('path');
+
+const mainOpts = {
   size: {
     alias: 's',
     describe: 'size of the requested image. (1-2048)'
@@ -17,80 +19,77 @@ var mainOpts = {
     describe: 'default image when no profile image found. [choices: "404", "mm", "identicon", "monsterid", "wavatar", "retro", "blank", "an image url"]'
   }
 };
-var setUsage = function(yargs){
+const setUsage = function (yargs) {
   return yargs
-      .options(mainOpts)
-      .help('h')
-      .alias('h', 'help')
-      .alias('v', 'version')
-      .version(function() { return 'gravatar version: ' + require('./package').version; })
-      .describe('v', 'show version information')
-      .epilogue('Useful Links:' +
-          '\n- https://en.gravatar.com/site/implement/images/' +
-          '\n- https://en.gravatar.com/site/implement/profiles/'+
-          '\n- https://github.com/emerleite/node-gravatar'
-        )
-
+    .options(mainOpts)
+    .help('h')
+    .alias('h', 'help')
+    .alias('v', 'version')
+    .version(() => path.join('gravatar version: ', require('./package').version))
+    .describe('v', 'show version information')
+    .epilogue(`Useful Links:
+        \n- https://en.gravatar.com/site/implement/images/
+        \n- https://en.gravatar.com/site/implement/profiles/
+        \n- https://github.com/emerleite/node-gravatar`);
 };
 
-var yargs = setUsage(require('yargs'))
-    .usage('Usage: $0 command somebody@example.com [options]')
-    .command('avatar', 'avatar somebody@example.com  [options]', mainOpts)
-    .command('profile', 'profile somebody@example.com  [options]', {
-      format: {
-        alias: 'f',
-        describe: 'format of the requested profile url',
-        choices: ['json', 'xml', 'qr', 'php', 'vcf']
-      },
-      callback: {
-        alias: 'c',
-        describe: 'name of a callback function when using json profile url eg. doSomething'
-      }
-    })
-    .example('$0 somebody@example.com')
-    .example('$0 avatar somebody@example.com')
-    .example('$0 profile somebody@example.com')
+const yargs = setUsage(require('yargs'))
+  .usage('Usage: $0 command somebody@example.com [options]')
+  .command('avatar', 'avatar somebody@example.com  [options]', mainOpts)
+  .command('profile', 'profile somebody@example.com  [options]', {
+    format: {
+      alias: 'f',
+      describe: 'format of the requested profile url',
+      choices: ['json', 'xml', 'qr', 'php', 'vcf']
+    },
+    callback: {
+      alias: 'c',
+      describe: 'name of a callback function when using json profile url eg. doSomething'
+    }
+  })
+  .example('$0 somebody@example.com')
+  .example('$0 avatar somebody@example.com')
+  .example('$0 profile somebody@example.com');
 
-var argv = yargs.argv;
-var command = argv._[0];
-var email = argv._[1];
+const argv = yargs.argv;
+const command = argv._[0];
+const email = argv._[1];
 
-var getOptions = function(argv){
-  var options = {};
+const getOptions = function (argv) {
+  const options = {};
   options.default = argv.d || argv.default;
   options.size = argv.s || argv.size;
-  options.protocol = argv.p || argv.protocol
-  options.format = argv.f || argv.format
-  options.callback = argv.c || argv.callback
+  options.protocol = argv.p || argv.protocol;
+  options.format = argv.f || argv.format;
+  options.callback = argv.c || argv.callback;
 
   // prune options
-  for (var prop in options) {
-    if(options.hasOwnProperty(prop) && !options[prop]){
+  for (const prop in options) {
+    if (options.hasOwnProperty(prop) && !options[prop]) {
       delete options[prop];
     }
   }
   return options;
 };
 
-function printAvatarUrl(email, options){
-  console.log('Gravatar (avatar):')
-  return gravatar.url(email, options) + '\n'
+function printAvatarUrl(email, options) {
+  console.log('Gravatar (avatar):');
+  return `${gravatar.url(email, options)}\n`;
 }
 
-var exec = function(argv){
-  var options = getOptions(argv);
-  if(command === 'profile'){
-    console.log('Gravatar (profile):')
-    return gravatar.profile_url(email, options) + '\n'
-  }else if(command === 'avatar' && emailValidator.validate(email)){
-    return printAvatarUrl(email, options)
-  }else if(emailValidator.validate(command)){
-    return printAvatarUrl(command, options)
-  }else{
-    yargs.showHelp();
-    return '';
-  }
+function printAvatarProfile(email, options) {
+  console.log('Gravatar (profile):');
+  return `${gravatar.profile_url(email, options)}\n`;
 }
 
-process.stdout.write( exec(argv) )
-process.exit()
+const exec = function (argv) {
+  const options = getOptions(argv);
+  if (command === 'profile') return printAvatarProfile(email, options);
+  if (command === 'avatar' && emailValidator.validate(email)) return printAvatarUrl(email, options);
+  if (emailValidator.validate(command)) return printAvatarUrl(command, options);
+  yargs.showHelp();
+  return '';
+};
+
+process.stdout.write(exec(argv));
+process.exit();
